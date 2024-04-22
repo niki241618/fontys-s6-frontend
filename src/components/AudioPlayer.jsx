e2cbb7f43f2a9ea236d4a965d5522c328aa14fa8
Nikolay Valchanov <n.valchanov@student.fontys.nl>
2024-04-22 15:52:58 +0200

    Added e2e test for the browse books page

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -1,11 +1,10 @@
-// import {getClientAudioFiles} from "../API/axiosClient";
 import useFetch from "../hooks/useFetch";
 import {useMemo} from "react";
 import LoadingSpinner from "./LoadingSpinner";
-import {getClient} from "../API/axiosClient";
+
 
 const AudioPlayer = ({fileName}) => {
-    const { data: audioFile, error, isLoading} = useFetch(`/streaming/${fileName}`,getClient(), true, {responseType: 'blob'})
+    const { data: audioFile, error, isLoading} = useFetch(`/streaming/${fileName}`, true, {responseType: 'blob'})
 
     const memoizedAudioFile = useMemo(() => audioFile, [audioFile]);
 
